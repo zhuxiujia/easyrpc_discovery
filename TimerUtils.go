da8a1322751ebb841fdb52c9525114aaae915f27
zhuxiujia <zhuxiujia@qq.com>
2019-05-18 00:03:17 +0800

    优化

--- TimerUtils.go
+++ TimerUtils.go
@@ -18,9 +18,6 @@ const (
 	Execute_coroutine
 )
 
-var timerStarted = false
-var jobChain = []func(){}
-
 //启动Timer,调用它请不要使用 go 关键字,内部已经使用协程处理
 func StartTimer(startType StartType, exeType ExecuteType, d time.Duration, fn func()) {
 	if startType == StartType_Now {
@@ -34,19 +31,8 @@ func StartTimer(startType StartType, exeType ExecuteType, d time.Duration, fn fu
 	}
 }
 func run(fn func(), d time.Duration) {
-	jobChain = append(jobChain, fn)
-	if timerStarted == true {
-		return
-	}
-	timerStarted = true
 	ticker := time.NewTicker(d)
 	for _ = range ticker.C {
-		for _, v := range *getJob() {
-			v()
-		}
+		fn()
 	}
-}
-
-func getJob() *[]func() {
-	return &jobChain
 }
