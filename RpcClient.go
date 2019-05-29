fef1d7cbdf0946e5709c5fc5f777f22b4e0976cb
zhuxiujia <zhuxiujia@qq.com>
2019-05-29 20:29:58 +0800

    add conn pool

--- RpcClient.go
+++ RpcClient.go
@@ -1,16 +1,18 @@
 package easyrpc_discovery
 
-import "github.com/zhuxiujia/easyrpc"
-
 type RpcClient struct {
-	Address           string      //远程地址
-	Object            interface{} //rpc对象
+	Address           string //远程地址
 	Pool              *ConnPool
 	LoadBalanceClient *RpcLoadBalanceClient
+	ShutDown          bool
 }
 
-func (it *RpcClient) Call(serviceMethod string, args interface{}, reply interface{}) error {
-	var e = it.Object.(*easyrpc.Client).Call(serviceMethod, args, reply)
+func (it *RpcClient) Call(serviceName string, serviceMethod string, args interface{}, reply interface{}) error {
+	var c, e = it.Pool.GetAndPush(serviceName, it.Address)
+	if e != nil {
+		return e
+	}
+	e = c.Call(serviceMethod, args, reply)
 	if e != nil && e.Error() == ConnError {
 		it.Close()
 	}
@@ -18,13 +20,11 @@ func (it *RpcClient) Call(serviceMethod string, args interface{}, reply interfac
 }
 
 func (it *RpcClient) Close() {
-	if it.Object != nil {
-		it.Object.(*easyrpc.Client).Close()
-	}
 	if it.LoadBalanceClient != nil {
 		it.LoadBalanceClient.Delete(it.Address, nil)
 	}
 	if it.Pool != nil {
 		it.Pool.Pop(it.Address)
 	}
+	it.ShutDown = true
 }
